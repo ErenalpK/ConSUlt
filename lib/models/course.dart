9709bed244b3c03660f6bc4eb0c99e90438f8dc2
selimyilmaz0 <selim.yilmaz@sabanciuniv.edu>
2025-12-22 13:46:35 +0300

    Update course.dart

--- lib/models/course.dart
+++ lib/models/course.dart
@@ -1,34 +1,40 @@
 class Course {
   final String id;
-  final String courseId;
-  final String courseName;
+  final String code;
+  final String name;
   final String faculty;
   final String department;
   final String description;
   final int ects;
   final String instructor;
+  final List<dynamic> prerequisites;
+  final List<dynamic> corequisites;
 
   Course({
     required this.id,
-    required this.courseId,
-    required this.courseName,
+    required this.code,
+    required this.name,
     required this.faculty,
     required this.department,
     required this.description,
     required this.ects,
     required this.instructor,
+    required this.prerequisites,
+    required this.corequisites,
   });
 
   factory Course.fromFirestore(Map<String, dynamic> data, String id) {
     return Course(
       id: id,
-      courseId: data['courseId'],
-      courseName: data['courseName'],
-      faculty: data['faculty'],
-      department: data['department'],
-      description: data['description'],
-      ects: data['ects'],
-      instructor: data['instructor'],
+      code: data['code'] ?? data['courseId'] ?? id,
+      name: data['name'] ?? data['courseName'] ?? '',
+      faculty: data['faculty'] ?? '',
+      department: data['department'] ?? '',
+      description: data['description'] ?? '',
+      ects: data['ects'] ?? 0,
+      instructor: data['instructor'] ?? '',
+      prerequisites: data['prerequisites'] ?? [],
+      corequisites: data['corequisites'] ?? [],
     );
   }
 }
